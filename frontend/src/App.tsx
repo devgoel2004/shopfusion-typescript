1dcde6b999c4a2123c51e22943bea8c6c20ce21b
devgoel2004 <devgoel12072004@gmail.com>
2026-07-13 23:55:15 +0530

    Added Header file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home/Home';
+import { Header } from './components/Layout/Header/Header';
+import { Footer } from './components/Layout/Footer/Footer';
 function App() {
   return (
+    <>
     <BrowserRouter>
+    <Header isAuthenticated= {false}/>
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
     </BrowserRouter>
+    <Footer></Footer>
+    </>
   )
 }
 
--- frontend/src/components/Layout/Header/Header.tsx
+++ frontend/src/components/Layout/Header/Header.tsx
@@ -1,6 +1,126 @@
 import {Button} from '@mui/material';
-export const Header = ()=>{
+import './Header.css';
+import React, {useState, useEffect} from 'react';
+import image from '../../../assets/images/ecommerce.png'
+import { Link,useNavigate } from 'react-router-dom';
+
+type HeaderProps = {
+    isAuthenticated: boolean,
+}
+export const Header = ({isAuthenticated}:HeaderProps)=>{
+    const navigate = useNavigate();
+    const[keyword, setKeyword] = useState<string>("");
+
+    const searchSubmitHandler = (e: React.FormEvent<HTMLFormElement>):void=>{
+        e.preventDefault();
+        if(keyword.trim()){
+            navigate(`/shopfusion/products/${keyword}`);
+        }else{
+            navigate(`/shopfusion/products`);
+        }
+    }
+    const[isActive, setIsActive] = useState<boolean>(false);
+    const toggleActiveClass = ():void=>{
+        setIsActive(!isActive);
+    }
+    const removeActive = ():void=>{
+        setIsActive(true);
+    }
+    useEffect(()=>{
+        const handleResize = ()=>{
+            if(window.innerWidth > 780){
+                removeActive();
+            }
+        };
+        window.addEventListener("resize", handleResize);
+        handleResize();
+        return ()=>{
+            window.removeEventListener("resize",handleResize);
+        }
+    },[]);
     return <>
-    <Button>Hello</Button>
+    <>
+      <div className="Header-section-1">
+        <div className="heading">
+          <i
+            onClick={toggleActiveClass}
+            className="fa-solid fa-bars"
+            id="bar-icon"></i>
+          <Link to="/shopfusion/">
+            <img src={image} alt="Shop" />
+            <b>Shop</b>
+            <span>Fusion</span>
+          </Link>
+        </div>
+        <div className="searchbox">
+          <form onSubmit={searchSubmitHandler}>
+            <input
+              type="text"
+              placeholder="Search..."
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setKeyword(e.target.value)}
+            />
+            <button type="submit">
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </form>
+        </div>
+        <div className="list">
+          <ul>
+            <li>About Us</li>
+            <li>Blog</li>
+            <li>Contact Us</li>
+            <li>Help & Support</li>
+          </ul>
+        </div>
+        <div className="icons">
+          <a href="https://www.instagram.com/__dev__goel__/">
+            <i className="fa-brands fa-instagram"></i>
+          </a>
+          <a href="https://www.instagram.com/__dev__goel__/">
+            <i className="fa-brands fa-facebook"></i>
+          </a>
+          <a href="https://x.com/devgoel12072004">
+            <i className="fa-brands fa-twitter"></i>
+          </a>
+        </div>
+      </div>
+      <div className={isActive ? `Header-section-2` : `Header-section-3`}>
+        <div className="Categories">
+          <span style={{ fontSize: "large" }}>
+            <Link to="/shopfusion/products">
+              <i className="fa-solid fa-list" style={{ padding: "4px" }}></i>
+              Products
+            </Link>
+          </span>
+          <span id="hide">
+            <i className="fa-solid fa-arrow-down"></i>
+            Rs
+          </span>
+          <span id="hide">
+            <i className="fa-solid fa-arrow-down"></i>
+            English
+          </span>
+        </div>
+        <div className="Categories">
+          <span>
+            <Link
+              to={
+                isAuthenticated ? "/shopfusion/account" : "/shopfusion/login"
+              }>
+              <i className="fa-solid fa-user"></i>
+              {isAuthenticated ? " Account" : " Sign In"}
+            </Link>
+          </span>
+          <span>
+            <i className="fa-solid fa-bag-shopping"></i> Favourite
+          </span>
+          <span>
+            <Link to="/shopfusion/cart">
+              <i className="fa-solid fa-bag-shopping"></i> Cart
+            </Link>
+          </span>
+        </div>
+      </div>
+    </>
     </>
 }
