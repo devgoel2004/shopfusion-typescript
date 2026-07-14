d04c49ce0efb9a91b98670970a6db9611613be7f
devgoel2004 <devgoel12072004@gmail.com>
2026-07-14 22:40:24 +0530

    added redux constants

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,5 +1,13 @@
+import React, {useEffect} from "react";
+import './Home.css';
+import {FaMouse} from 'react-icons/fa';
+import Loader from "../Loader/Loader";
+import {useAlert} from 'react-alert';
+import {useSelector, useDispatch} from 'react-redux';
 
 function Home(){
+    const alert = useAlert();
+    const dispatch = useDispatch();
     return (
         <>
           Home
