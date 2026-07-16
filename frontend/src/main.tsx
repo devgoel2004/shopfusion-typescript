f30904fd98052bbab1ba314a3eee5ad089fca821
devgoel2004 <devgoel12072004@gmail.com>
2026-07-17 00:04:25 +0530

    Added react-alert-template-basic and it's template for typescript

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import "@fortawesome/fontawesome-free/css/all.min.css";
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
+import { positions, transitions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
+const options = {
+  timeout: 2000,
+  position: positions.BOTTOM_CENTER,
+  transition: transitions.SCALE,
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
+  <Provider store={store}>
+    <AlertProvider template={AlertTemplate} {...options}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </AlertProvider>
+  </Provider>
+);
---
+++ frontend/src/react-alert-template-basic.d.ts
@@ -0,0 +1,7 @@
+declare module "react-alert-template-basic" {
+  import type { ComponentType, ReactElement } from "react";
+  import type { AlertTemplateProps } from "react-alert";
+
+  const AlertTemplate: ComponentType<AlertTemplateProps>;
+  export default AlertTemplate;
+}
